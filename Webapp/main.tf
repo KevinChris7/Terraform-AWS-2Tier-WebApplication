31295a4a146ed5d6747a06a7101f4e34e0f95f15
Kevin Christopher <kevinchristopherkc7@gmail.com>
2020-06-17 12:58:03 +0530

    Added Data sources of Infrastructure to main.tf

--- Webapp/main.tf
+++ Webapp/main.tf
@@ -1,6 +1,42 @@
+########### Data Source - Infrastructure ###########
+
+### VPC Data ###
+data "aws_vpc" "vpc_input"{
+  default = false
+}
+
+### Public Subnets ###
+data "aws_subnet_ids" "subnet_ids_pub_input"{
+  vpc_id = data.aws_vpc.vpc_input
+  tags {
+    Name = "Public"
+  }
+}
+
+### Private Subnet ###
+data "aws_subnet" "subnet_id_priv_input"{
+  vpc_id = data.aws_vpc.vpc_input
+  tags {
+    Name = "Private"
+  }
+}
+
+### Application Security Group ###
+data "aws_security_group" "secgp_cjkapp_input"{
+  vpc_id = data.aws_vpc.vpc_input
+  filter {
+    name = "name"
+    values = "cjkwebgrp"
+  }
+}
+
+
+########### Application Resources ###########
+
+### Security Group of ELB ###
 resource "aws_security_group" "sg_elb" {
   name   = "sg_elb"
-  vpc_id = aws_vpc.ivpc.id
+  vpc_id = data.aws_vpc.vpc_input.id
   ingress {
     description = "HTTP from VPC"
     from_port   = 80
@@ -22,21 +58,24 @@ resource "aws_security_group" "sg_elb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-//load balancer
+
+### ELB ###
 resource "aws_lb" "cjkalb" {
   name               = "cjkalb"
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg_elb.id]
-  subnet_mapping {
-    subnet_id = aws_subnet.ipublicsub.id
-  }
+  subnets            = (data.aws_subnet_ids.subnet_ids_pub_input.ids)
 }
+
+### ELB Target Group ###
 resource "aws_lb_target_group" "alb-tg" {
   name     = "cjkalb-tg"
   port     = "80"
   protocol = "HTTP"
-  vpc_id   = aws_vpc.ivpc.id
+  vpc_id   = data.aws_vpc.vpc_input.id
 }
+
+### ELB Listener ###
 resource "aws_lb_listener" "alb-list-http" {
   load_balancer_arn = aws_lb.cjkalb.arn
   port              = "80"
@@ -46,6 +85,8 @@ resource "aws_lb_listener" "alb-list-http" {
     target_group_arn = aws_lb_target_group.alb-tg.arn
   }  
 }
+
+### ELB Listener ###
 resource "aws_lb_listener" "alb-list-https" {
   load_balancer_arn = aws_lb.cjkalb.arn
   port              = "443"
@@ -55,18 +96,21 @@ resource "aws_lb_listener" "alb-list-https" {
     target_group_arn = aws_lb_target_group.alb-tg.arn
   }  
 }
+
+### ELB Attachment ###
 resource "aws_lb_target_group_attachment" "alb-ec2-attach" {
   target_group_arn = aws_lb_target_group.alb-tg.arn
   target_id        = aws_instance.webapp.id
   port             = 80
 }
 
+### EC2 Web Instance ###
 resource "aws_instance" "webapp" {
   ami                    = lookup(var.AMIS, var.AWS_REGION)
   instance_type          = var.INSTANCE_TYP
   key_name               = var.KEYPAIR
-  vpc_security_group_ids = [aws_security_group.cjkwebgrp.id]
-  subnet_id              = aws_subnet.iprivatesub.id
+  vpc_security_group_ids = data.aws_security_group.secgp_cjkapp_input.id
+  subnet_id              = data.aws_subnet.subnet_ids_priv_input.id
   tags = {
     Name = "CJKAPP"
   }
